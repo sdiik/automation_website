0d5dd296347d1620384c45ee7aa375ffa005baba
shella_20 <shellagabriella212@gmail.com>
2024-03-04 20:34:39 +0700

    push mapping element for Delete Product page

---
+++ Object Repository/Daftar Jual Page/btn_productCard.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_productCard</name>
+   <tag></tag>
+   <elementGuidId>81e5c2a0-9203-4d0f-9510-17c1b8f2dcc6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.card.px-0.border-0.shadow.h-100.pb-4.rounded-4</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Tambah Produk'])[1]/following::div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>3b8fbff7-ef12-4ef2-b735-a8e90820a86e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card px-0 border-0 shadow h-100 pb-4 rounded-4</value>
+      <webElementGuid>8867d198-cdba-4fc7-9bf8-bfe639b9cb07</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      
+
+    
+      test produk baju
+      Baju
+      Rp 123
+    
+  </value>
+      <webElementGuid>7b65eb31-aadb-40cd-84bd-09047b42b1ac</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row my-5&quot;]/div[@class=&quot;col-12 col-lg-9 mt-5 mt-lg-0&quot;]/div[@class=&quot;row g-4&quot;]/div[@class=&quot;col-12 col-lg-4&quot;]/a[@class=&quot;product-card&quot;]/div[@class=&quot;card px-0 border-0 shadow h-100 pb-4 rounded-4&quot;]</value>
+      <webElementGuid>35c6aa06-3cb9-41f9-bdde-4d041e612fd4</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Tambah Produk'])[1]/following::div[2]</value>
+      <webElementGuid>788ada2f-c072-4d84-a7a5-04dae77df372</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual'])[1]/following::div[6]</value>
+      <webElementGuid>46a7a8ee-e390-4379-853b-febb5d42b51a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/a/div</value>
+      <webElementGuid>ad8fd29b-a839-467f-bbe5-023eb92e52e4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+      
+
+    
+      test produk baju
+      Baju
+      Rp 123
+    
+  ' or . = '
+      
+
+    
+      test produk baju
+      Baju
+      Rp 123
+    
+  ')]</value>
+      <webElementGuid>3cc4cb0d-a7b6-4243-bf74-0d37217ac9e4</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
