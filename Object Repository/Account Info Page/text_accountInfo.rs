ca23a5c012adbec01e34997f97d0baaee9d0d8c2
shella_20 <shellagabriella212@gmail.com>
2024-03-09 17:04:01 +0700

    Add text_accountInfo for verifying purpose

---
+++ Object Repository/Account Info Page/text_accountInfo.rs
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>text_accountInfo</name>
+   <tag></tag>
+   <elementGuidId>3b5156a2-48a1-4518-a97b-f812fa390389</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Nama'])[1]/preceding::div[5]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.fs-6.fw-bold.text-center.position-absolute.top-50.start-50.translate-middle</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>98780d50-53f4-4142-9c66-9fa821262a7e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fs-6 fw-bold text-center position-absolute top-50 start-50 translate-middle</value>
+      <webElementGuid>b1db92ed-b088-48eb-afa3-9f263a690d0e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+        Lengkapi Info Akun
+      </value>
+      <webElementGuid>ae1b0ad5-1862-4ee1-ae8b-076a34a3d36d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/nav[@class=&quot;navbar navbar-expand-lg navbar-light bg-white shadow fixed-top&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;fs-6 fw-bold text-center position-absolute top-50 start-50 translate-middle&quot;]</value>
+      <webElementGuid>e1d8c3fe-e6ac-4a67-baa1-4523db799803</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Nama'])[1]/preceding::div[5]</value>
+      <webElementGuid>5a4dc618-46e3-4c55-838e-f9df817271a5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kota'])[1]/preceding::div[6]</value>
+      <webElementGuid>daca9dd8-81b1-4734-aa83-f041b9d1484b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Lengkapi Info Akun']/parent::*</value>
+      <webElementGuid>b22899ae-3d68-46a3-a231-20613f3a69ca</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div</value>
+      <webElementGuid>f595c261-a676-4695-9365-3dd7268ff51f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+        Lengkapi Info Akun
+      ' or . = '
+        Lengkapi Info Akun
+      ')]</value>
+      <webElementGuid>df30f3b9-5ab3-4a76-909f-d042ce859f34</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
