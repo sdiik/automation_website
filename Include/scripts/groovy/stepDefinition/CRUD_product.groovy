2f01d5deb8443a23669bd45094721c23dbae7940
tamshidayat <tamshidayat@gmail.com>
2024-03-18 14:22:57 +0700

    push automation and gherkin script for post preview product in CRUD feature

---
+++ Include/scripts/groovy/stepDefinition/CRUD_product.groovy
@@ -0,0 +1,104 @@
+package stepDefinition
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import internal.GlobalVariable
+
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
+
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.util.KeywordUtil
+
+import com.kms.katalon.core.webui.exception.WebElementNotFoundException
+
+import cucumber.api.java.en.And
+import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+
+
+
+class CRUD_product {
+	//Post and preview page
+	@Given("user has opened selling page")
+	public void user_has_opened_selling_page() {
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_jual'))
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Post Product Page/btn_terbitkan'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Post Product Page/btn_terbitkan'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Post Product Page/btn_preview'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Post Product Page/btn_preview'), 0)
+	}
+
+	@When("user input valid credential in all required fields user and select (.*) in selling page")
+	public void user_input_valid_credential_in_all_required_fields_user_and_select_in_selling_page(String button) {
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_namaProduk'), 'beat')
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_hargaProduk'), '1000')
+		WebUI.selectOptionByValue(findTestObject('Object Repository/Post Product Page/select_pilihKategori'), '2', false)
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_deskripsiProduk'), 'barang bekas')
+		WebUI.uploadFile(findTestObject('Object Repository/Post Product Page/img_formImage'), 'C:\\Users\\WINDOWS 10\\Downloads\\sapi.jpg')
+		WebUI.delay(1)
+		if(button=="terbitkan") {
+			WebUI.click(findTestObject('Object Repository/Post Product Page/btn_terbitkan'))
+		}else if (button=="preview") {
+			WebUI.click(findTestObject('Object Repository/Post Product Page/btn_preview'))
+		}
+	}
+	@Then("user will successfully (.*) product")
+	public void user_will_successfully_product(String result) {
+		if(result=="post") {
+			WebUI.waitForElementPresent(findTestObject('Object Repository/Product Details Page/btn_delete'), 0)
+			WebUI.verifyElementPresent(findTestObject('Object Repository/Product Details Page/btn_delete'), 0)
+			WebUI.waitForElementPresent(findTestObject('Object Repository/Product Details Page/btn_edit'), 0)
+			WebUI.verifyElementPresent(findTestObject('Object Repository/Product Details Page/btn_edit'), 0)
+		}else if(result=="preview") {
+			WebUI.waitForElementPresent(findTestObject('Object Repository/Preview Page/btn_terbitkan'), 0)
+			WebUI.verifyElementPresent(findTestObject('Object Repository/Preview Page/btn_terbitkan'), 0)
+			WebUI.waitForElementPresent(findTestObject('Object Repository/Preview Page/btn_edit'), 0)
+			WebUI.verifyElementPresent(findTestObject('Object Repository/Preview Page/btn_edit'), 0)
+		}
+	}
+
+	//user doesn't choose category
+	@When("user input valid credential in all required except in kategori fields and click terbitkan button in selling page")
+	public void user_input_valid_credential_in_all_required_except_in_kategori_fields_and_click_terbitkan_button_in_selling_page() {
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_namaProduk'), 'beat')
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_hargaProduk'), '1000')
+		WebUI.selectOptionByLabel(findTestObject('Object Repository/Post Product Page/select_pilihKategori'), 'Pilih Kategori', false)
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_deskripsiProduk'), 'barang bekas')
+		WebUI.uploadFile(findTestObject('Object Repository/Post Product Page/img_formImage'), 'C:\\Users\\WINDOWS 10\\Downloads\\sapi.jpg')
+		WebUI.click(findTestObject('Object Repository/Post Product Page/btn_terbitkan'))
+	}
+
+	@Then("user will fail post new product")
+	public void user_will_fail_post_new_product() {
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Post Product Page/text_errorMessage_kategori'), 0)
+		WebUI.closeBrowser()
+	}
+
+}
---
+++ Object Repository/post product page/text_errorMessage_kategori.rs
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>text_errorMessage_kategori</name>
+   <tag></tag>
+   <elementGuidId>303ccd09-dc89-42da-aab0-a8934dc27e3e</elementGuidId>
+   <imagePath></imagePath>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Kategori'])[1]/following::div[1]</value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.form-text.text-danger</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>f6ffdd33-df33-4fb7-887d-d70f9b173062</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-text text-danger</value>
+      <webElementGuid>0c343c98-278d-49d9-aee3-7d5d15e79030</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            Category must exist
+          </value>
+      <webElementGuid>07e18ab7-ba39-4872-a1a3-36f17cd2b696</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/section[@class=&quot;container container-sm position-relative&quot;]/section[@class=&quot;row mt-5&quot;]/div[@class=&quot;col-11&quot;]/form[1]/div[@class=&quot;mb-4&quot;]/div[@class=&quot;form-text text-danger&quot;]</value>
+      <webElementGuid>a6f150cb-e2e6-416e-89ea-a3e08b462240</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kategori'])[1]/following::div[1]</value>
+      <webElementGuid>daa10c4b-2da5-43ce-b028-9a3f4aada944</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Harga Produk'])[1]/following::div[2]</value>
+      <webElementGuid>483d8dc5-1ea4-41c7-8ff1-f6a70282e581</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Deskripsi'])[1]/preceding::div[1]</value>
+      <webElementGuid>c51618c5-09cb-4ec6-937d-3244c15daa13</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=concat('Description can', &quot;'&quot;, 't be blank')])[1]/preceding::div[3]</value>
+      <webElementGuid>916db164-2e0c-4fd3-bc49-a079fb07c8c2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Category must exist']/parent::*</value>
+      <webElementGuid>dd424f6d-9266-43c9-8cf4-10f453f96b1e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div</value>
+      <webElementGuid>686601fe-b207-468f-9888-266cfac45d48</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+            Category must exist
+          ' or . = '
+            Category must exist
+          ')]</value>
+      <webElementGuid>c57d07b2-82cf-4d56-bf0e-838cdb1a4d29</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
