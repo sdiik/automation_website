6581a7505d499897841d71ca8bba4a65f8d60ee0
Archimawan <Archiandro@hotmail.com>
2024-03-19 12:49:30 +0700

    Add gherkrin and automation script for Seller Transaction page

--- Include/scripts/groovy/stepDefinition/Transaction.groovy
+++ Include/scripts/groovy/stepDefinition/Transaction.groovy
@@ -8,6 +8,8 @@ import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
 
 import java.security.PublicKey
 
+import javax.xml.soap.SAAJResult
+
 import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.checkpoint.Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
@@ -35,7 +37,7 @@ public class Transaction {
 		WebUI.verifyElementPresent(findTestObject('Object Repository/Register page/btn_daftar'), 0)
 		WebUI.waitForElementPresent(findTestObject('Object Repository/Register page/TextBox_name'), 0)
 		WebUI.verifyElementPresent(findTestObject('Object Repository/Register page/TextBox_name'), 0)
-		
+
 		int RandomNumber;
 		RandomNumber = (int)(Math.random()*1000)
 
@@ -44,7 +46,7 @@ public class Transaction {
 		WebUI.setText(findTestObject('Object Repository/Register page/TextBox_password'), '123')
 		WebUI.click(findTestObject('Object Repository/Register page/btn_daftar'))
 	}
-	
+
 	@Given("buyers (.*) completed their profiles")
 	public void buyer_completed_their_profiles(String condition) {
 		if(condition=="have not") {
@@ -52,15 +54,15 @@ public class Transaction {
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
 			WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_profile'), 0)
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_profile'), 0)
-	
+
 			WebUI.click(findTestObject('Object Repository/Homepage/btn_profile'))
 			WebUI.click(findTestObject('Object Repository/Homepage/btn_accountInfo'))
-	
+
 			WebUI.waitForElementPresent(findTestObject('Object Repository/Account Info Page/TextArea_nama'), 0)
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Account Info Page/TextArea_nama'), 0)
 			WebUI.waitForElementPresent(findTestObject('Object Repository/Account Info Page/btn_back'), 0)
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Account Info Page/btn_back'), 0)
-	
+
 			WebUI.click(findTestObject('Object Repository/Account Info Page/btn_back'))
 		}
 		else if(condition=="have") {
@@ -68,15 +70,15 @@ public class Transaction {
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
 			WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_profile'), 0)
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_profile'), 0)
-	
+
 			WebUI.click(findTestObject('Object Repository/Homepage/btn_profile'))
 			WebUI.click(findTestObject('Object Repository/Homepage/btn_accountInfo'))
-	
+
 			WebUI.waitForElementPresent(findTestObject('Object Repository/Account Info Page/TextArea_nama'), 0)
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Account Info Page/TextArea_nama'), 0)
 			WebUI.waitForElementPresent(findTestObject('Object Repository/Account Info Page/btn_submit'), 0)
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Account Info Page/btn_submit'), 0)
-	
+
 			WebUI.uploadFile(findTestObject('Object Repository/Account Info Page/btn_profilePicture'), 'C:\\Users\\archi_000\\Pictures\\Angsa Kucing.PNG')
 			WebUI.setText(findTestObject('Object Repository/Account Info Page/TextArea_nama'), 'Archi Edited')
 			WebUI.selectOptionByValue(findTestObject('Object Repository/Account Info Page/dropdown_kota'), '4', false)
@@ -84,13 +86,11 @@ public class Transaction {
 			WebUI.setText(findTestObject('Object Repository/Account Info Page/TextArea_noHP'), '085814205153')
 			WebUI.click(findTestObject('Object Repository/Account Info Page/btn_submit'))
 		}
-		
-		
 	}
-	
+
 	@When ("buyers input (.*) values and send the trade offer")
 	public void buyers_input_values_and_send_the_trade_offer(String condition_two) {
-		
+
 		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
 		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
 
@@ -111,17 +111,16 @@ public class Transaction {
 		WebUI.verifyElementPresent(findTestObject('Object Repository/Buyer Transaction Page/TextBox_offer'), 0)
 		WebUI.waitForElementPresent(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'), 0)
 		WebUI.verifyElementPresent(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'), 0)
-		
+
 		if(condition_two=="correct") {
-		WebUI.setText(findTestObject('Object Repository/Buyer Transaction Page/TextBox_offer'), '1200')
-		WebUI.click(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'))
+			WebUI.setText(findTestObject('Object Repository/Buyer Transaction Page/TextBox_offer'), '1200')
+			WebUI.click(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'))
 		}
 		else if(condition_two=="string") {
-		WebUI.setText(findTestObject('Object Repository/Buyer Transaction Page/TextBox_offer'), 'ABC')
+			WebUI.setText(findTestObject('Object Repository/Buyer Transaction Page/TextBox_offer'), 'ABC')
 		}
-		
 	}
-	
+
 	@Then("buyers will (.*) in sending the trade offer")
 	public void buyers_will_in_sending_the_trade_offer(String result) {
 		if(result=="failed") {
@@ -137,6 +136,158 @@ public class Transaction {
 		else if(result=="fail inputting string") {
 			WebUI.waitForElementPresent(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'), 0)
 			WebUI.verifyElementPresent(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'), 0)
+		}
+		WebUI.closeBrowser()
+	}
+	
+	@Given("seller succesfully post product to the website")
+	public void seller_succesfully_post_product_to_the_website() {
+		
+		WebUI.openBrowser('https://secondhand.binaracademy.org/users/sign_in')
+		WebUI.maximizeWindow()
+		
+		//Seller open and successfully login
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Login Page/TextArea_email'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Login Page/TextArea_email'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Login Page/TextArea_password'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Login Page/TextArea_password'), 0)
+		WebUI.setText(findTestObject('Object Repository/Login Page/TextArea_email'),'ArchiSeller@mail.com')
+		WebUI.setText(findTestObject('Object Repository/Login Page/TextArea_password'),'123')
+		WebUI.click(findTestObject('Object Repository/Login Page/btn_loginCommit'))
+		
+		//Seller enter home page
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_profile'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_profile'), 0)
+		
+		//Seller open the selling page
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_jual'))
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Post Product Page/btn_terbitkan'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Post Product Page/btn_terbitkan'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Post Product Page/btn_preview'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Post Product Page/btn_preview'), 0)
+		
+		//Seller submit new product for sale
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_namaProduk'), 'beat')
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_hargaProduk'), '1000')
+		WebUI.selectOptionByValue(findTestObject('Object Repository/Post Product Page/select_pilihKategori'), '2', false)
+		WebUI.setText(findTestObject('Object Repository/Post Product Page/TextBox_deskripsiProduk'), 'barang bekas')
+		WebUI.uploadFile(findTestObject('Object Repository/Post Product Page/img_formImage'), 'C:\\Users\\archi_000\\Pictures\\milkcat.PNG')
+		WebUI.delay(1)
+		WebUI.click(findTestObject('Object Repository/Post Product Page/btn_terbitkan'))
+		
+		//Seller logout
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_profile'))
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_keluar'))
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_masuk'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_masuk'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
+		
+	}
+	
+	@Given("buyers succesfully send the trade offer")
+	public void buyers_succesfully_send_the_trade_offer() {
+		
+		//buyers open and successfully login
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_masuk'))
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Login Page/TextArea_email'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Login Page/TextArea_email'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Login Page/TextArea_password'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Login Page/TextArea_password'), 0)
+		WebUI.setText(findTestObject('Object Repository/Login Page/TextArea_email'),'ArchiBuyer@mail.com')
+		WebUI.setText(findTestObject('Object Repository/Login Page/TextArea_password'),'123')
+		WebUI.click(findTestObject('Object Repository/Login Page/btn_loginCommit'))
+		
+		//Buyers enter home page
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/card_product'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/card_product'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_profile'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_profile'), 0)
+		
+		//Buyers choose products
+		WebUI.scrollToElement(findTestObject('Object Repository/Homepage/card_product'), 0)
+		WebUI.click(findTestObject('Object Repository/Homepage/card_product'))
+		
+		//Buyers enter product pages
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Buyer Transaction Page/text_sellerInfo'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Buyer Transaction Page/text_sellerInfo'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Buyer Transaction Page/btn_tertarik_nego'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Buyer Transaction Page/btn_tertarik_nego'), 0)
+		WebUI.click(findTestObject('Object Repository/Buyer Transaction Page/btn_tertarik_nego'))
+		
+		//Buyers send offers
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Buyer Transaction Page/TextBox_offer'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Buyer Transaction Page/TextBox_offer'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'), 0)
+		WebUI.setText(findTestObject('Object Repository/Buyer Transaction Page/TextBox_offer'), '1200')
+		WebUI.click(findTestObject('Object Repository/Buyer Transaction Page/btn_kirim'))
+		
+		//Buyers logout
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_profile'))
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_keluar'))
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_masuk'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_masuk'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
+		
+	}
+	
+	@When("seller open trade offer notification")
+	public void seller_open_trade_offer_notification() {
+		
+		//buyers open and successfully login
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_masuk'))
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Login Page/TextArea_email'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Login Page/TextArea_email'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Login Page/TextArea_password'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Login Page/TextArea_password'), 0)
+		WebUI.setText(findTestObject('Object Repository/Login Page/TextArea_email'),'ArchiSeller@mail.com')
+		WebUI.setText(findTestObject('Object Repository/Login Page/TextArea_password'),'123')
+		WebUI.click(findTestObject('Object Repository/Login Page/btn_loginCommit'))
+		
+		//buyers click notification
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_notif'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_notif'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Homepage/btn_jual'), 0)
+		WebUI.click(findTestObject('Object Repository/Homepage/btn_notif'))
+		WebUI.click(findTestObject('Object Repository/Homepage/item_productOffer'))
+		
+	}
+	
+	@When("seller (.*) the offer")
+	public void seller_the_offer(String condition) {
+		
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Seller Transaction Page/label_Terima'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Seller Transaction Page/label_Terima'), 0)
+		WebUI.waitForElementPresent(findTestObject('Object Repository/Seller Transaction Page/label_Tolak'), 0)
+		WebUI.verifyElementPresent(findTestObject('Object Repository/Seller Transaction Page/label_Tolak'), 0)
+		
+		if(condition=="accepts") {
+			WebUI.click(findTestObject('Object Repository/Seller Transaction Page/label_Terima'))
+			WebUI.click(findTestObject('Object Repository/Seller Transaction Page/button_Status'))
+			WebUI.click(findTestObject('Object Repository/Seller Transaction Page/bullet_status_finished'))
+			WebUI.click(findTestObject('Object Repository/Seller Transaction Page/btn_kirim_transaksi'))
+		}
+		else if(condition=="rejects") {
+			WebUI.click(findTestObject('Object Repository/Seller Transaction Page/label_Tolak'))
+		}
+		
+	}
+	
+	@Then("seller will be succesfull in (.*) the transaction")
+	public void seller_will_be_succesfull_in_the_transaction(String result) {
+		if(result=="accepting") {
+			
+			WebUI.waitForElementPresent(findTestObject('Object Repository/Seller Transaction Page/text_accepted'), 0)
+			WebUI.verifyElementPresent(findTestObject('Object Repository/Seller Transaction Page/text_accepted'), 0)
+		}
+		else if (result=="rejecting") {
+			WebUI.waitForElementPresent(findTestObject('Object Repository/Seller Transaction Page/text_rejected'), 0)
+			WebUI.verifyElementPresent(findTestObject('Object Repository/Seller Transaction Page/text_rejected'), 0)
 		}
 		WebUI.closeBrowser()
 	}
---
+++ Object Repository/Seller Transaction Page/text_rejected.rs
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>text_rejected</name>
+   <tag></tag>
+   <elementGuidId>62a7d39b-2ed1-4e45-a25c-2c213efbcdef</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h6.offer-status.text-black-50</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Daftar Produkmu yang Ditawar'])[1]/following::h6[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>div >> internal:has-text=&quot;Penawaran produk ditolak 19 Mar, 12:13 beat Rp 1.000 Ditawar Rp 1.200&quot;i >> h6</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h6</value>
+      <webElementGuid>bf00f578-341a-4f1c-ba91-6d276696d887</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>offer-status text-black-50</value>
+      <webElementGuid>1413b22e-7384-450d-a52f-102345ade7db</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+              Penawaran produk ditolak
+            </value>
+      <webElementGuid>c18de13b-4fae-46a4-a869-c51f088611ba</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/section[@class=&quot;container-sm&quot;]/ul[@class=&quot;offers mt-5&quot;]/li[@class=&quot;offer gap-4 mt-5&quot;]/div[@class=&quot;offer-content&quot;]/div[@class=&quot;offer-header&quot;]/h6[@class=&quot;offer-status text-black-50&quot;]</value>
+      <webElementGuid>d3e526f1-b77b-4a9e-9c65-3feac572ef00</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Daftar Produkmu yang Ditawar'])[1]/following::h6[1]</value>
+      <webElementGuid>8cbd6c3e-7fa9-4849-bb29-aa579f612786</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Solo'])[1]/following::h6[1]</value>
+      <webElementGuid>2dd37a3a-cb11-4423-8b1e-c6613a8370aa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='beat'])[1]/preceding::h6[1]</value>
+      <webElementGuid>f3826ac6-fe52-4bdb-80e8-6cb9ee4b5169</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Rp 1.000'])[1]/preceding::h6[1]</value>
+      <webElementGuid>6ee2d176-85c0-4ff4-9b4a-c229de48dff9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Penawaran produk ditolak']/parent::*</value>
+      <webElementGuid>3d86b4d9-7049-4d62-8ec8-9c4696f3df16</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h6</value>
+      <webElementGuid>34aca485-f89a-4287-b7c7-de7888ee6e32</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h6[(text() = '
+              Penawaran produk ditolak
+            ' or . = '
+              Penawaran produk ditolak
+            ')]</value>
+      <webElementGuid>a16bc00b-af50-4266-8a9f-d3f9ec596328</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
