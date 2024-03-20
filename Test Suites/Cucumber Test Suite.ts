e34cf76045c86db8a4bbc9964710c63c56798d84
Archimawan <Archiandro@hotmail.com>
2024-03-20 22:55:57 +0700

    Add changes to Transaction feature and other project

---
+++ Test Suites/Cucumber Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Cucumber Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>017e14ac-ecb7-4b52-9c4e-61e2dce74f45</testSuiteGuid>
+   <testCaseLink>
+      <guid>7523830b-8a5e-49f5-a46e-fe316c93f6d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Cucumber</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
